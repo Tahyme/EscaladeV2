1d18a694c5cde6ca264c65a7f747a97f05069661
Elaquivala <bombertime4motion@gmail.com>
2023-02-01 15:12:29 +0100

    Test Dashboard user

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,7 +2,9 @@
  
 namespace App\Controller;
  
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
  
@@ -18,4 +20,22 @@ class DashboardController extends AbstractController
             'controller_name' => 'DashboardController',
         ]);
     }
+
+    /*public function createProduct(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $User = new User();
+        $User->getId();
+        $User->getEmail();
+        $User->getRoles();
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($User);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$User->getId());
+    }*/
 }
